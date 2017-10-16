cb52c4f59d360703b35f310cbdc4a78b6901afde
Jeffrey Harmon <jeffrey@mordtech.com>
2017-10-16 17:26:43 +0200

    Fixed !FC

--- cmds/Fc.js
+++ cmds/Fc.js
@@ -1,5 +1,5 @@
 var db = require('../dbAccess.js')
-const fcPattern = new RegExp(/^(\d{4}-\d{4}-\d{4})/g)
+const fcPattern = /\d{4}-\d{4}-\d{4}/
 
 this.info = {
   aliases: [
@@ -24,17 +24,16 @@ this.Command = function (data) {
     } else { // If no entry, do this
       return `${data.user.username} haven't set a Friend Code yet!\n To add a friend code, do \`!fc [insert your FC here]\`!`
     }
-  } else if (data.args.length === 1) { // If there is args
-    let regexCheck = fcPattern.test(data.args[0]) // Boolean. Check if message contains a correct FC
+  } else  { // If there is args
+    let regexCheck = fcPattern.test(data.args.join(' ')) // Boolean. Check if message contains a correct FC
     if (regexCheck) { // If true
-      let FC = fcPattern.exec(data.args[0]) // Make FC the result of FCPattern
-      db.setUserInfo(data.user.id, {'FriendCode': data.args[0]})
+      let FC = fcPattern.exec(data.args.join(' ')) // Make FC the result of FCPattern
+      db.setUserInfo(data.user.id, {'FriendCode': FC})
 
       // Setting role
       data.message.member.addRole(data.message.guild.roles.find('name', 'Villager'), 'Added friend code')
       data.message.member.removeRole(data.message.guild.roles.find('name', 'Newbie'), 'Added friend code')
-      return ' Your Friend Code is now `' + data.args[0] + '`'
-      // TODO: Store data to db, add / remove roles, error handling message
+      return ' Your Friend Code is now `' + FC + '`'
     } else {
       return ' Invalid Friend Code or User! \n The code format should be `xxxx-xxxx-xxxx`'
     }
